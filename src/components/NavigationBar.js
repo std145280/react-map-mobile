e17b3a16152124c9d8ad8d8f5df4f4ae292a1598
std145280 <std145280@ac.eap.gr>
2020-12-20 23:20:53 +0200

    navigation bar changes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -29,80 +29,17 @@ export default function NavigationBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-
-            <NavDropdown title="Vehicles" id="collasible-nav-dropdown">
-              <NavDropdown.Item 
-                href="#all_vehicles"
-                onClick={() => history.push("/Vehicles")}
-              >
-                All Vehicles
-              </NavDropdown.Item>
-            
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                href="#add_vehicle"
-                onClick={() => history.push("/AddVehicle")}
-              >
-                Add Vehicle
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Tour Guides" id="collasible-nav-dropdown">
-              <NavDropdown.Item
-                href="#all_tour_guides"
-                onClick={() => history.push("/TourGuides")}
-              >
-                All Tour Guides
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                href="#add_tour_guide"
-                onClick={() => history.push("/AddTourGuide")}
-              >
-                Add Tour Guide
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Points Of Interest" id="collasible-nav-dropdown">
-              <NavDropdown.Item
-                href="#all_points_of_interest"
-                onClick={() => history.push("/PointOfInterest")}
-              >
-                All Points Of Interest
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                href="#add_point_of_interest"
-                onClick={() => history.push("/AddPointOfInterest")}
-              >
-                Add Point of Interest
-              </NavDropdown.Item>
-
-            </NavDropdown>
-            <NavDropdown title="Tours" id="collasible-nav-dropdown">
-              <NavDropdown.Item
-                href="#all_tours"
-                onClick={() => history.push("/Tours")}
-              >
-                All Tours
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                href="#add_tour"
-                onClick={() => history.push("/AddTour")}
-              >
-                Add Tour
-              </NavDropdown.Item>
-            </NavDropdown>
-            
-          </Nav>
-          <Nav>
-            <Nav.Link
+{/*
+          <Nav.Link
               href="#rent_requests"
               onClick={() => history.push("/RentRequests")}
             >
               Rent Requests
-            </Nav.Link>
+</Nav.Link> */}
+            
+          </Nav>
+          <Nav>
+            
             <NavDropdown title="Settings" id="collasible-nav-dropdown">
               <NavDropdown.Item
                 href="#settings"
