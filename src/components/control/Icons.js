04a98b094010d8da7733a2455dbcb31817b1335f
std145280 <std145280@ac.eap.gr>
2020-12-22 02:11:16 +0200

    new icons for start and finish

--- src/components/control/Icons.js
+++ src/components/control/Icons.js
@@ -2,6 +2,10 @@ import L from 'leaflet';
 import simpleMarker from "../../images/marker-icon.png"
 import carIcon from "../../images/car-icon.png"
 
+import start from "../../images/startIcon.png"
+import finish from "../../images/finishIcon.png"
+
+
 const markerIcon = L.icon({
     iconUrl: simpleMarker,
     iconAnchor: [5, 25],
@@ -16,4 +20,19 @@ const markerIcon = L.icon({
     iconSize: [25, 25],
   });
 
-  export { markerIcon, carMarkerIcon };
+  const startIcon = L.icon({
+    iconUrl: start,
+    iconAnchor: [5, 25],
+    popupAnchor: [5, -20],
+    iconSize: [25, 25],
+  });
+
+
+  const finishIcon = L.icon({
+    iconUrl: finish,
+    iconAnchor: [5, 25],
+    popupAnchor: [5, -20],
+    iconSize: [25, 25],
+  });
+
+  export { markerIcon, carMarkerIcon, finishIcon, startIcon };
