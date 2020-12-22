b344b3314c046c9f36949e797efb1923d4b1d255
std145280 <std145280@ac.eap.gr>
2020-12-22 20:21:48 +0200

    LeafletMapTour cleanup

--- src/components/control/LeafletMapTour.js
+++ src/components/control/LeafletMapTour.js
@@ -1,20 +1,6 @@
 import React from "react";
-import {
-  Map,
-  Marker,
-  Popup,
-  TileLayer,
-  ZoomControl,
-  ScaleControl,
-} from "react-leaflet";
 import L from "leaflet";
-import { markerIcon, startIcon, finishIcon, carMarkerIcon } from "./Icons";
-
-const height = { height: "100vh" };
-var center = { lat: 37.9838, lng: 23.7275 };
-
-var geoLatlng;
-var locationString;
+import { markerIcon, startIcon, finishIcon } from "./Icons";
 
 class LeafletMapTour extends React.Component {
   constructor(props) {
@@ -25,27 +11,19 @@ class LeafletMapTour extends React.Component {
   center = { lat: this.props.lonitude, lng: this.props.langitude };
 
   componentDidMount() {
-    //const map = this.leafletMap.leafletElement;
     const geocoder = L.Control.Geocoder.nominatim();
-
     var map = L.map("map", {
       zoom: 10,
     });
     var markerArray = [];
-
     markerArray.push(L.latLng(this.props.startLatlng));
-
-
     for (let i = 0; i < this.props.tour.poi.length; i++) {
       markerArray.push(
         L.latLng(this.props.tour.poi[i].geoLat, this.props.tour.poi[i].geoLng)
       );
     }
-
     markerArray.push(L.latLng(this.props.finishLatlng));
-
     map.setView([this.props.tour.geoLat, this.props.tour.geoLong], 10);
-
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -53,12 +31,11 @@ class LeafletMapTour extends React.Component {
 
     var routeControl = L.Routing.control({
       waypoints: markerArray,
-
       createMarker: function (i, wp, nWps) {
         if (i === 0 ) {
           return L.marker(wp.latLng, {
             icon: startIcon,
-            draggable: true,
+            draggable: true
           });
         } else if( i === nWps - 1) {          return L.marker(wp.latLng, {
           icon: finishIcon,
@@ -70,28 +47,13 @@ class LeafletMapTour extends React.Component {
         }
       },
     }).addTo(map);
-
     routeControl.route();
-
-    routeControl.on("routesfound", function (e) {
-      var routes = e.routes;
-      var summary = routes[0].summary;
-      // alert time and distance in km and minutes
-      alert(
-        "Total distance is " +
-          summary.totalDistance / 1000 +
-          " km and total time is " +
-          summary.totalTime / 60 +
-          " minutes"
-      );
-      //  map.removeControl();
-    });
   }
 
   render() {
     return (
       <>
-        <h3>hi</h3>
+        <div id="map">L.map('leafletmap')</div>
       </>
     );
   }
